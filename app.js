346d42166ee28957da503c6e6ed5aeee4ddfc762
Tom Tilly <tomtillistrand@gmail.com>
2019-04-01 21:43:30 -0400

    add comments new and create routes

--- app.js
+++ app.js
@@ -5,37 +5,40 @@ const bodyParser = require('body-parser');
 const Recipe = require('./models/recipe');
 const Comment = require('./models/comment');
 const seedDB = require('./seed');
-
-seedDB();
-
-const port = 7777;
 require('dotenv').config({ path: 'variables.env' });
+const port = 7777;
 
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
+seedDB();
 
 // Database setup
 
 mongoose.connect('mongodb://localhost:27017/omnomnom', { useNewUrlParser: true});
 
+
+/* Routes */
+
+// Home
 app.get('/', function(req, res){
 	res.redirect('/recipes');
 });
 
+// Index
 app.get('/recipes', function(req, res){	
 	Recipe.find({}, function(err, recipes){
 		if(err){
 			console.log(err);
 		} else {
-			res.render('index', {recipes: recipes});
+			res.render('recipes/index', {recipes: recipes});
 		}
 	});
 });
 
+// Create
 app.post('/recipes', function(req, res){
 	const name = req.body.name;
 	const ingredients = req.body.ingredients.split(/\r?\n/g);
@@ -48,26 +51,59 @@ app.post('/recipes', function(req, res){
 			console.log(err);
 		} else {
 			console.log(req.body);
-			res.redirect('/index');		
+			res.redirect('recipes/index');		
 		}
 	});
 });
 
+// New
 app.get('/recipes/new', function(req, res){
-	res.render('new');
+	res.render('recipes/new');
 });
 
-
+// Show
 app.get('/recipes/:id', function(req, res){
-	Recipe.findById(req.params.id, function(err, foundRecipe){
+	Recipe.findById(req.params.id).populate('comments').exec(function(err, foundRecipe){
 		if(err){
 			console.log(err);
 		} else {
-			res.render('show', {recipe: foundRecipe});
+			res.render('recipes/show', {recipe: foundRecipe});
 		}
 	});
 });
 
+// ============
+// Comments Routes
+// ============
+
+// New - Comments
+app.get('/recipes/:id/comments/new', function(req, res){
+	Recipe.findById(req.params.id, function(err, foundRecipe){
+		res.render('comments/new', {recipe: foundRecipe});
+	});
+});
+
+// Create - Comments
+app.post('/recipes/:id/comments', function(req, res){
+	Recipe.findById(req.params.id, function(err, recipe){
+		if(err){
+			console.log(err);
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					recipe.comments.push(comment);
+					recipe.save();
+					res.redirect(`/recipes/${recipe._id}`);
+				}
+			});
+		}
+	});
+	
+});
+
+// 404
 app.get('*', function(req, res){
 	res.send('404 - Page Not Found');
 });
