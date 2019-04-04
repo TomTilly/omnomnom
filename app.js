57d079f1ea545b6a5b3a28fc50a35e193ed31028
Tom Tilly <tomtillistrand@gmail.com>
2019-04-03 20:28:34 -0400

    prepend __dirname to /public path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 7777;
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 seedDB();
 
